41e3c0920da1000585d69da38405f006772ff489
PyDevC <anuskcse@gmail.com>
2025-01-21 19:20:57 -0500

    feat: git functions

--- git_folder_clone.sh
+++ git_folder_clone.sh
@@ -1,35 +1,85 @@
 #!/bin/bash
-#####################################
-# REWRITE ACCORDING TO func IN HELP #
-#####################################
-if [ $# -eq 1 ]; then
-    dir="docs" 
-elif [ $# -eq 3 ]; then
-    dir="$2"
-    branch="$3"
-else 
-    dir="$2"
-fi
 
-url="$1"
-###################################
+file=""
+url=""
+branch=""
+dir=""
+method=""
+
+# doesn't work for shuffled arguments
+# fix not working
+# maybe traversal will help
+while [ $# -gt 0 ]; do
+    protocol=("${1//// }") 
+    protocol=($(echo $protocol | tr " " " " ))
+    protocol="${protocol[0]}"
+
+    if [ "$protocol" == "https:" ]; then
+        url="$1"
+    elif [ "$2" != "" ]; then
+        dir="$2"
+    elif [ "$3" != "" ]; then
+        branch="$3"
+    elif [ "$4" != ""  ]; then
+        method="$4"
+    else
+        echo "wrong arguments check help"
+    fi
+    shift
+done
 
 help(){
     desc="A useful git script for multi-purpose"
     func="[url] [sub-directory] [branch] [method]"
     func_help="Print this description"
-    fucn_no_checkout="uses git --no-checkout. It prevents the auto download of all files from the git"
+    func_no_checkout="uses git --no-checkout. It prevents the auto download of all files from the git"
+    func_archive="uses git archive. It downloads just the folder without cloning"
+    func_simple_clone="get the clone for repo."
+    func_partial_clone="creates a partial clone using --filter=blob:none. This is helpful for less download data"
     warnings=""
 
+    echo $desc
+    echo $func
+    echo "--help" $func_help
+    echo "--no-checkout --no-chk" $func_no_checkout
+    echo "--archive" $func_archive
+    echo "--partial" $func_partial_clone
+    echo "no argument passes" $func_simple_clone
+}
+
+no_checkout(){
+    git clone --no-checkout "$url"
+    cd "$file" 
+    git sparse-checkout init
+    git sparse-checkout set "$dir"
+    git checkout "$branch"
+}
+
+archive(){
+    # method doesn't work for https protocol 
+    # find alternative solution
+    git archive --remote=$url HEAD:$dir | tar -x
+}
+
+simple_clone(){
+    git clone "$url"
+}
+
+partial_clone(){
+    git clone --filter=blob:none "$url"
+    cd "$file"
+    git sparse-checkout init
+    git sparse-checkout set "$dir"
+    git checkout "$branch"
 }
 
 ##########
 # CHANGE #
 ###################################
-file=("${url//// }") 
-file=($(echo $file | tr " " " " ))
-file="${file[3]}"
-file="${file%.git}"
+file=("${url//// }") # returns the url as spaced individul
+file=($(echo $file | tr " " " " )) # converts file string to array
+file="${file[3]}" # gets the git repo name
+file="${file%.git}" # gets the repo name
 ##################################
 
 ##################
@@ -39,22 +89,27 @@ echo file: "$file"
 echo url: "$url"
 echo branch: "$branch"
 echo dir: "$dir"
+echo method: "$method"
 ##################################
 
-no_checkout(){
-    # url dir file branch
-    git clone --no-checkout "$url"
-    cd "$file" 
-    git sparse-checkout init
-    git sparse-checkout set "$dir"
-    git checkout "$branch"
-}
-
-archive(){
-    mkdir $file - $dir
-    git archive --remote=$url HEAD:$dir
-}
-
-simple_clone(){}
-
-partial_clone(){}
+# method 
+case "$method" in
+    "--help")
+        help
+        ;;
+    "--no-chk" | "--no-checkout")
+        no_checkout
+        ;;
+    "--archive")
+        archive
+        ;;
+    "--partial")
+        partial_clone
+        ;;
+    "")
+        simple_clone
+        ;;
+    *)
+        echo "not a method"  
+        ;;
+esac
