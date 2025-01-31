eff2237d7afba6c64f65b194ddcf12776bb10d82
PyDevC <anuskcse@gmail.com>
2025-01-31 08:48:27 -0500

    updated readme

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ You can freely customize the functions according to your need. It is recommended
 to use these with tmux, neovim and other utilities to increase you productivity.
 Now added navigation support using fzf it helps to quickly jump between directories.
 
-## contrib
-You can provide your suggestions or report a bug by raising issue.
+## CONTRIB
+you can provide your suggestions or report a bug by raising issue.
 To contibute check [TEMPLATE.md](contrib/TEMPLATE.md) and 
 [FEATURE.md](contrib/FEATURE.md)
+
+## WARNINGS
+
+warnings about scripts
+
+`run.sh`<br>
+don't use run.sh in the same directory as it will result in continuous execution 
+of the script, causing an infinite loop which can onlybe undone by closing the 
+terminal. **Ctrl-c won't do a thing.
+
--- run.sh
+++ run.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-scripts_dir="$(cd "$(dirname ${BASH_SOURCE[0]} &> /dev/null && pwd)")"
+scripts_dir=$(dirname "${BASH_SOURCE[0]}" &> /dev/null && pwd)
 
 dry="0"
 grep=""
+analyze="0"
 
-run_dir=`find $script_dir/runs -mindepth 1 -maxdepth 1 -executable`
+run_dir=`find $script_dir -mindepth 1 -maxdepth 1 -executable`
+echo $scripts_dir
+echo $run_dir
+
+readfile(){
+    file="$2"
+    comment="#|"
+    if [[ -f $file ]]; then
+        while read -r line;do
+            if [[ $line == $comment* ]]; then
+                printf '%s\n' "$line"
+            fi
+        done < $file
+    fi
+}
 
 log(){
     if [[ "$dry" == "1" ]]; then
         echo "[ DRY RUN ]: " $1
+    elif [[ "$analyze" == "1" ]];then
+        echo "[ ANALYZE ]: " $1
+        readfile $1
     else
         echo "$1"
     fi
@@ -18,6 +36,8 @@ log(){
 while [[ $# -gt 0 ]]; do 
     if [[ "$1" == "--dry" ]]; then
         dry="1"
+    elif [[ $1 == "--dry-analyze" ]]; then
+        analyze="1"
     else
         grep="$1"
     fi
@@ -32,7 +52,7 @@ for s in $run_dir; do
     fi
 
     log "running $s"
-    if [[ $dry == "0" ]]; then
+    if [[ $dry == "0" && $analyze == "0" ]]; then
         $s
     fi
 done
